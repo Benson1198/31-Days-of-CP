4bb126efc94ec8a0dd03e3e8ae22c4ab1db75ad8
Benson P Xavier <34964177+Benson1198@users.noreply.github.com>
2020-05-29 22:25:16 +0530

    28

---
+++ Day 29/Sum of Digits(GFG).py
@@ -0,0 +1,24 @@
+def sumOfDigits(n):
+    lst = [int(y) for y in list(str(n))]
+    add = sum(lst)
+    return add
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        print(sumOfDigits(n))
